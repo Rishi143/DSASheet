9e025859314dae8e1bfb2d55cc261074e786ff12
Rishi143 <rishikeshreddykarnati@gmail.com>
2022-07-22 15:46:42 +0530

    some more code

---
+++ src/main/java/com/dsa/overall/_5BestTimeToBuyAndSellStock.java
@@ -0,0 +1,16 @@
+package com.dsa.overall;
+
+public class _5BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+        int totalProfit = 0;
+        int len = prices.length;
+        for(int i = 1;i < len;i++) {
+            if(prices[i] > prices[i-1]) {
+                totalProfit += prices[i] - prices[i-1];
+            }
+        }
+        return totalProfit;
+    }
+
+}
